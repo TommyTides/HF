39d83e1a358be9b15a0af54dc43ac8bdcbf23645
Tommy Tides <54915266+TommyTides@users.noreply.github.com>
2024-06-20 12:46:55 +0200

    Update shoppingcart.php

--- app/views/cart/shoppingcart.php
+++ app/views/cart/shoppingcart.php
@@ -13,7 +13,7 @@ include __DIR__ . '/../cart_header.php';
                 </div>
                 <div class="item-name"><?php echo $item['name']; ?></div>
                 <div class="item-quantity">
-                    <input type="number" value="<?php echo $item['quantity']; ?>" min="1">
+                    <input id="quantity-<?= $item->getProductId() ?>" type="number" value="<?php echo $item['quantity']; ?>" min="1">
                 </div>
                 <div class="item-price">
                     <span class="original-price">$<?php echo $item['price']; ?></span>
@@ -30,10 +30,10 @@ include __DIR__ . '/../cart_header.php';
             <span class="label">Subtotal:</span>
             <span class="value">$<?php echo $subtotal; ?></span>
         </div>
-        <div class="tax">
+        <!-- <div class="tax">
             <span class="label">Tax:</span>
             <span class="value">$<?php echo $tax; ?></span>
-        </div>
+        </div> -->
         <div class="total">
             <span class="label">Total:</span>
             <span class="value">$<?php echo $total; ?></span>
